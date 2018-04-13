26cd092018fb935f6d7b465631afc2f46f7c988a
ivan limongan <ivan.limongan@gmail.com>
2018-04-12 23:06:59 -0700

    initial setup working

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Testing'
+    }
+  }
+}
+</script>
+
+<style>
+.clip {
+  position: absolute !important;
+  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+}
+</style>
